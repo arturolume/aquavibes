26fbd288e5ef4aee8165b9d36a4461bb69fadd56
arturolumev <edwin.lume@tecsup.edu.pe>
2023-10-07 20:32:25 -0500

    style: primer cambio

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Bienvenidos a AquaVibes</h1>
+        <h1 class="text-4xl font-bold">Bienvenido a AquaVibes</h1>
         <p class="my-4">
           En desarrollo &#128578;
         </p>
